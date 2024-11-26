39e8c93534569cf9458d5587529fc3ec787306b6
Cephelo <79186646+Cephelo@users.noreply.github.com>
2024-11-26 14:55:25 -0600

    Update DespawnAfterTime.java
    integrated addTotalMassFromDespawns statistic

--- src/main/java/com/github/sculkhorde/common/entity/goal/DespawnAfterTime.java
+++ src/main/java/com/github/sculkhorde/common/entity/goal/DespawnAfterTime.java
@@ -45,5 +45,6 @@ public class DespawnAfterTime extends Goal {
     {
         ((Mob)mob).remove(Entity.RemovalReason.DISCARDED);
         SculkHorde.savedData.addSculkAccumulatedMass((int) ((Mob) mob).getHealth());
+        SculkHorde.statisticsData.addTotalMassFromDespawns((int) ((Mob) mob).getHealth()); 
     }
 }
