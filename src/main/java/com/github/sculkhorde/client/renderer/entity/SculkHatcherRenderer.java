eaf1663fab1e4e5a489a60f4ed9d21655fdd0b2e
Mike <mrpkeleven+github@gmail.com>
2024-12-14 14:15:47 -0500

    Updated Sculk Hatcher Assets
    Added new model, texture, and glowmask to hatcher

--- src/main/java/com/github/sculkhorde/client/renderer/entity/SculkHatcherRenderer.java
+++ src/main/java/com/github/sculkhorde/client/renderer/entity/SculkHatcherRenderer.java
@@ -4,10 +4,12 @@ import com.github.sculkhorde.client.model.enitity.SculkHatcherModel;
 import com.github.sculkhorde.common.entity.SculkHatcherEntity;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
 public class SculkHatcherRenderer extends GeoEntityRenderer<SculkHatcherEntity> {
 
     public SculkHatcherRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new SculkHatcherModel());
+        this.addRenderLayer(new AutoGlowingGeoLayer(this));
     }
 }
