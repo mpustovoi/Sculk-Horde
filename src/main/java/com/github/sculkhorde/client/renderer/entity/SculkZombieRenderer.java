7bfd21c14777ddbcbf2fc535b6f8de2866fb44f0
Mike <mrpkeleven+github@gmail.com>
2024-12-13 21:13:09 -0500

    Updated Sculk Zombie Texture & Model
    Sculk Zombie has new texture, model, and now glows in the dark.

--- src/main/java/com/github/sculkhorde/client/renderer/entity/SculkZombieRenderer.java
+++ src/main/java/com/github/sculkhorde/client/renderer/entity/SculkZombieRenderer.java
@@ -1,9 +1,10 @@
 package com.github.sculkhorde.client.renderer.entity;
 
-import com.github.sculkhorde.common.entity.SculkZombieEntity;
 import com.github.sculkhorde.client.model.enitity.SculkZombieModel;
+import com.github.sculkhorde.common.entity.SculkZombieEntity;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
 
 public class SculkZombieRenderer extends GeoEntityRenderer<SculkZombieEntity> {
@@ -11,6 +12,7 @@ public class SculkZombieRenderer extends GeoEntityRenderer<SculkZombieEntity> {
 
     public SculkZombieRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new SculkZombieModel());
+        this.addRenderLayer(new AutoGlowingGeoLayer(this));
     }
 
 }
