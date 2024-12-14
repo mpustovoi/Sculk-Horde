7c55752d2c9301f5aec9e941d7d952c34621f6f5
Michael Pena-Knight <mrpkeleven+github@gmail.com>
2024-12-14 17:38:41 -0500

    Updated Sculk Creeper Assets
    Updated texture, model, and glowmask

--- src/main/java/com/github/sculkhorde/client/renderer/entity/SculkCreeperRenderer.java
+++ src/main/java/com/github/sculkhorde/client/renderer/entity/SculkCreeperRenderer.java
@@ -4,6 +4,7 @@ import com.github.sculkhorde.client.model.enitity.SculkCreeperModel;
 import com.github.sculkhorde.common.entity.SculkCreeperEntity;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
 
 public class SculkCreeperRenderer extends GeoEntityRenderer<SculkCreeperEntity> {
@@ -11,6 +12,7 @@ public class SculkCreeperRenderer extends GeoEntityRenderer<SculkCreeperEntity> 
 
     public SculkCreeperRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new SculkCreeperModel());
+        this.addRenderLayer(new AutoGlowingGeoLayer(this));
     }
 
 }
