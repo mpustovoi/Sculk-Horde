bfc06651fdeb8bab8e8f62322466d4e2d68262ff
Mike <mrpkeleven+github@gmail.com>
2024-12-14 22:24:43 -0500

    Updated Assets for Sculk Spore Spewer
    Updated Texture and glowmask

--- src/main/java/com/github/sculkhorde/client/renderer/entity/SculkSporeSpewerRenderer.java
+++ src/main/java/com/github/sculkhorde/client/renderer/entity/SculkSporeSpewerRenderer.java
@@ -4,12 +4,14 @@ import com.github.sculkhorde.client.model.enitity.SculkSporeSpewerModel;
 import com.github.sculkhorde.common.entity.SculkSporeSpewerEntity;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
 public class SculkSporeSpewerRenderer extends GeoEntityRenderer<SculkSporeSpewerEntity> {
 
     public SculkSporeSpewerRenderer(EntityRendererProvider.Context renderManager)
     {
         super(renderManager, new SculkSporeSpewerModel());
+        this.addRenderLayer(new AutoGlowingGeoLayer(this));
     }
 
 }
