14b931f2cc74b796a8ad1b1be0a1fe09881c4278
Mike <mrpkeleven+github@gmail.com>
2024-12-16 00:40:43 -0500

    Updated Sculk Witch Assets
    Updated Model, texture, and glowmask

--- src/main/java/com/github/sculkhorde/client/renderer/entity/SculkWitchRenderer.java
+++ src/main/java/com/github/sculkhorde/client/renderer/entity/SculkWitchRenderer.java
@@ -4,6 +4,7 @@ import com.github.sculkhorde.client.model.enitity.SculkWitchModel;
 import com.github.sculkhorde.common.entity.SculkWitchEntity;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
 
 public class SculkWitchRenderer extends GeoEntityRenderer<SculkWitchEntity> {
@@ -11,6 +12,7 @@ public class SculkWitchRenderer extends GeoEntityRenderer<SculkWitchEntity> {
 
     public SculkWitchRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new SculkWitchModel());
+        this.addRenderLayer(new AutoGlowingGeoLayer(this));
     }
 
 }
