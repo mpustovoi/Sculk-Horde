78d142b6937fe4c1651f13f907647a4ba903f662
Mike <mrpkeleven+github@gmail.com>
2024-12-16 14:41:05 -0500

    Updated Sculk Ravager Assets
    Updated Mode, Texture, and glowmask

--- src/main/java/com/github/sculkhorde/client/renderer/entity/SculkRavagerRenderer.java
+++ src/main/java/com/github/sculkhorde/client/renderer/entity/SculkRavagerRenderer.java
@@ -4,11 +4,13 @@ import com.github.sculkhorde.client.model.enitity.SculkRavagerModel;
 import com.github.sculkhorde.common.entity.SculkRavagerEntity;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
 public class SculkRavagerRenderer extends GeoEntityRenderer<SculkRavagerEntity>
 {
     public SculkRavagerRenderer(EntityRendererProvider.Context renderManager)
     {
         super(renderManager, new SculkRavagerModel());
+        this.addRenderLayer(new AutoGlowingGeoLayer(this));
     }
 }
