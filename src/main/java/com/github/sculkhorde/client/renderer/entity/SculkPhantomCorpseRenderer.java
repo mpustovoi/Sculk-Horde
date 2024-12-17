428f6023d3926e7e1bbc276b163b81cebda0e673
Mike <mrpkeleven+github@gmail.com>
2024-12-17 13:47:23 -0500

    Fixed Sculk Phantom Model & Animation

--- src/main/java/com/github/sculkhorde/client/renderer/entity/SculkPhantomCorpseRenderer.java
+++ src/main/java/com/github/sculkhorde/client/renderer/entity/SculkPhantomCorpseRenderer.java
@@ -1,17 +1,17 @@
 package com.github.sculkhorde.client.renderer.entity;
 
 import com.github.sculkhorde.client.model.enitity.SculkPhantomCorpseModel;
-import com.github.sculkhorde.client.model.enitity.SculkSporeSpewerModel;
 import com.github.sculkhorde.common.entity.SculkPhantomCorpseEntity;
-import com.github.sculkhorde.common.entity.SculkSporeSpewerEntity;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
 public class SculkPhantomCorpseRenderer extends GeoEntityRenderer<SculkPhantomCorpseEntity> {
 
     public SculkPhantomCorpseRenderer(EntityRendererProvider.Context renderManager)
     {
         super(renderManager, new SculkPhantomCorpseModel());
+        this.addRenderLayer(new AutoGlowingGeoLayer(this));
     }
 
 }
