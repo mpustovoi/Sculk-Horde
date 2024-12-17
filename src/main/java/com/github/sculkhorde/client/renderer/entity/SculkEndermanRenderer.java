021ee0cecbc84d540d5c06f2984d019199a1d2ae
Mike <mrpkeleven+github@gmail.com>
2024-12-17 12:17:41 -0500

    Updated Sculk Endemran Assets
    Updated model, texture, and glowmask.

--- src/main/java/com/github/sculkhorde/client/renderer/entity/SculkEndermanRenderer.java
+++ src/main/java/com/github/sculkhorde/client/renderer/entity/SculkEndermanRenderer.java
@@ -4,6 +4,7 @@ import com.github.sculkhorde.client.model.enitity.SculkEndermanModel;
 import com.github.sculkhorde.common.entity.boss.sculk_enderman.SculkEndermanEntity;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import software.bernie.geckolib.renderer.GeoEntityRenderer;
+import software.bernie.geckolib.renderer.layer.AutoGlowingGeoLayer;
 
 
 public class SculkEndermanRenderer extends GeoEntityRenderer<SculkEndermanEntity> {
@@ -11,6 +12,7 @@ public class SculkEndermanRenderer extends GeoEntityRenderer<SculkEndermanEntity
 
     public SculkEndermanRenderer(EntityRendererProvider.Context renderManager) {
         super(renderManager, new SculkEndermanModel());
+        this.addRenderLayer(new AutoGlowingGeoLayer(this));
     }
 
 }
